89b1087790cbf9ba1720bbd5c60f169249fe042e
kolya-t <toporkov.kolya96@gmail.com>
2017-02-01 22:58:40 +0300

    Added closing database connection when DBException throwing.

--- src/main/java/database/DBException.java
+++ src/main/java/database/DBException.java
@@ -1,10 +1,23 @@
 package database;
 
+import database.helper.Connector;
+
+import java.sql.SQLException;
+
 /**
  * Ошибка при выполнении операции с датабазным сервисом
  */
 public class DBException extends Exception {
+    /**
+     * При выбрасывании ошибки соединение с базой данных закрывается
+     * @param e
+     */
     public DBException(Exception e) {
         super(e);
+        try {
+            Connector.getConnection().close();
+        } catch (SQLException e1) {
+            e1.printStackTrace();
+        }
     }
 }
