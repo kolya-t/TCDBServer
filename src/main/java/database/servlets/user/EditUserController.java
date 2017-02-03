6448ab97021b1b6b9704b18825a4279d0ef6232e
kolya-t <toporkov.kolya96@gmail.com>
2017-02-03 19:27:05 +0300

    Controller to dispatch to page with input data for editing new user.

---
+++ src/main/java/database/servlets/user/EditUserController.java
@@ -0,0 +1,41 @@
+package database.servlets.user;
+
+import database.DBException;
+import database.DBService;
+import database.pojo.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/editUser")
+public class EditUserController extends HttpServlet {
+
+    private static final String SUCCESS_PAGE = "/views/editUserView.jsp";
+    private static final String ERROR_PAGE = "/userList";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        req.setCharacterEncoding("UTF-8");
+
+        String forward = SUCCESS_PAGE;
+        try {
+            long id = Long.parseLong(req.getParameter("id"));
+            User user = DBService.getInstance().getUser(id);
+            if (user == null) {
+                forward = ERROR_PAGE;
+            } else {
+                req.setAttribute("user", user);
+            }
+        } catch (NumberFormatException | DBException e) {
+            e.printStackTrace();
+            forward = ERROR_PAGE;
+        }
+        req.getRequestDispatcher(forward).forward(req, resp);
+    }
+}
