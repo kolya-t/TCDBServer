e58422e08288f6ebb764cc00beb84ea3ee9d19d5
kolya-t <toporkov.kolya96@gmail.com>
2017-02-03 19:26:42 +0300

    Controller to dispatch to page with input data for inserting new user.

---
+++ src/main/java/database/servlets/user/AddUserController.java
@@ -0,0 +1,20 @@
+package database.servlets.user;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/addUser")
+public class AddUserController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+        req.setCharacterEncoding("UTF-8");
+
+        req.getRequestDispatcher("/views/addUserView.jsp").forward(req, resp);
+    }
+}
