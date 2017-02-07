80ef61a13ac16d806ef2f0f70f9ec814ace277e0
kolya-t <toporkov.kolya96@gmail.com>
2017-02-07 15:34:47 +0300

    Added logout controller

---
+++ src/main/java/servlets/LogoutController.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+import static servlets.LoginController.USERNAME_IN_COOKIE;
+import static servlets.filters.CookieFilter.COOKIE_CHECKED;
+
+
+@WebServlet("/logout")
+public class LogoutController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        deleteLoggedUser(req.getSession());
+        deleteUserCookie(resp);
+        req.getSession().removeAttribute(COOKIE_CHECKED);
+        resp.sendRedirect("/");
+    }
+
+    public static void deleteUserCookie(HttpServletResponse resp) {
+        Cookie cookieUserName = new Cookie(USERNAME_IN_COOKIE, null);
+        cookieUserName.setMaxAge(0);
+        resp.addCookie(cookieUserName);
+    }
+
+    public void deleteLoggedUser(HttpSession session) {
+        session.removeAttribute("loggedUser");
+    }
+}
