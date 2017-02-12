654b37b34fb175535d326035535ff9a60915d6ba
kolya-t <toporkov.kolya96@gmail.com>
2017-02-12 22:30:29 +0300

    Deleted logic of pagination from controller

--- src/main/java/controllers/admin/ListController.java
+++ src/main/java/controllers/admin/ListController.java
@@ -25,27 +25,7 @@ public class ListController extends HttpServlet {
         req.setCharacterEncoding("UTF-8");
 
         try {
-            // создание списка пользователей
-            int offset = 0;
-            String offsetStr = req.getParameter("offset");
-            if (offsetStr != null) {
-                offset = Integer.parseInt(offsetStr);
-            }
-            req.setAttribute("userList", UserService.getInstance().getUserList(offset, PAGE_ROWS_LIMIT));
-
-            // создание списка номеров страниц и списка смещений
-            int rowCount = UserService.getInstance().getUserCount();
-            int pageCount = (int) Math.ceil((double) rowCount / PAGE_ROWS_LIMIT);
-            Map<Integer, Integer> offsets = new LinkedHashMap<>();
-            for (int i = 0; i < pageCount; i++) {
-                offsets.put(i + 1, i * PAGE_ROWS_LIMIT);   // смещения которые будут вставляться в ссылку
-            }
-            req.setAttribute("offsets", offsets);
-
-            // указание, какая страница активная
-            req.setAttribute("activePage", offset / PAGE_ROWS_LIMIT + 1);
-
-            // отрисовка страницы
+            req.setAttribute("userList", UserService.getInstance().getUserList());
             req.getServletContext().getRequestDispatcher(USER_LIST_PAGE_PATH).forward(req, resp);
         } catch (UserServiceException e) {
             e.printStackTrace();
