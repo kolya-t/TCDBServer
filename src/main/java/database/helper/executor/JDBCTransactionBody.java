8794aef8f1a72809280b28e78b1c2e3030b7c63a
kolya-t <toporkov.kolya96@gmail.com>
2017-02-13 17:57:15 +0300

    renamed: SQLTransactionBody -> JDBCTransactionBody

--- src/main/java/database/helper/executor/JDBCTransactionBody.java
+++ src/main/java/database/helper/executor/JDBCTransactionBody.java
@@ -3,7 +3,7 @@ package database.helper.executor;
 import java.sql.SQLException;
 import java.util.concurrent.Callable;
 
-public interface SQLTransactionBody<T> extends Callable<T> {
+public interface JDBCTransactionBody<T> extends Callable<T> {
 
     @Override
     T call() throws SQLException;
